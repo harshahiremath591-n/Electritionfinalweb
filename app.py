e6344abea41993684460d7271e394163c47d614f
harshahiremath367-a11y <harshahiremath367@gmail.com>
2026-05-14 09:48:07 +0530

    Fixed timedelta import

--- app.py
+++ app.py
@@ -1321,4 +1321,10 @@ def logout():
 # ================= RUN =================
 
 if __name__ == "__main__":
-    socketio.run(app, host="0.0.0.0", port=5000)
+    port = int(os.environ.get("PORT", 5000))
+    socketio.run(
+        app,
+        host="0.0.0.0",
+        port=port,
+        debug=False
+    )
